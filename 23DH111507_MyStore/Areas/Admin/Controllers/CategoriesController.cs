40d8c681f3dd7e58c10deb01c04eac31d98ea40f
khang <khang@LEGION-5>
2024-10-08 11:28:38 +0700

    Add project files.

---
+++ 23DH111507_MyStore/Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _23DH111507_MyStore.Areas.Admin.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        // GET: Admin/Categories
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ 23DH111507_MyStore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_23DH111507_MyStore.MvcApplication" Language="C#" %>
