e687666759ce81b14cd2e154f5062f36c2b497fc
phangkhan <166301575+phkgn@users.noreply.github.com>
2024-11-12 10:59:00 +0700

    update

---
+++ 23DH111507_MyStore/Models/ViewModel/ProductSearchVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PagedList.Mvc;
+
+namespace _23DH111507_MyStore.Models.ViewModel
+{
+    public class ProductSearchVM
+    {
+        public string SearchTerm {  get; set; }
+        public decimal? MinPrice { get; set; }
+        public decimal? MaxPrice { get; set; }
+        public string SortOder { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; } = 10;
+        public PagedList.IPagedList<Product> Products { get; set; }
+    }
+}
